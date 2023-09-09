5fc0f631098d732e4870d0176bd7e34e8e7f7772
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2023-09-09 18:34:43 +0530

    Dockerfile content for deployment

--- nextJS/blog-starter-app/next.config.js
+++ nextJS/blog-starter-app/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
-    output: 'export'
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    trailingSlash: true,
   }
    
   module.exports = nextConfig
