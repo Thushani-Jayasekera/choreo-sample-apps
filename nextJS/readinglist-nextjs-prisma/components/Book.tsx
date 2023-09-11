b64f6a0b33deb4ba9a38ed2f8ca33d28df8802ae
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2023-09-11 09:01:03 +0530

    Add icons

--- nextJS/readinglist-nextjs-prisma/components/Book.tsx
+++ nextJS/readinglist-nextjs-prisma/components/Book.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Router from "next/router";
-import { FcFullTrash,  FcEditImage} from "react-icons/fc";
+import { FcFullTrash, FcEditImage } from "react-icons/fc";
+import { useSession } from "next-auth/react";
 
 export type BookProps = {
   id: string;
@@ -10,28 +11,47 @@ export type BookProps = {
   user_writer: {
     name: string;
     email: string;
-  }
+  };
 };
 
 async function deleteBook(id: string): Promise<void> {
-    await fetch(`/api/book/${id}`, {
-      method: "DELETE",
-    });
-    alert("Book is deleted successfully");
-    Router.push("/");
-  }
+  await fetch(`/api/book/${id}`, {
+    method: "DELETE",
+  });
+  alert("Book is deleted successfully");
+  Router.push("/");
+}
 
 const Book: React.FC<{ book: BookProps }> = ({ book }) => {
-  const authorName = book.author? book.author : "Unknown author";
+  const session = useSession();
+  const user = session.data?.user;
+  const authorName = book.author ? book.author : "Unknown author";
   return (
     <div className="book-card">
-    <div className="book-div">
-      <h2>{book.name}</h2>
-      <small><i>Written By <b>{authorName}</b></i></small>
-    </div>
-    <div className="book-buttons">
-        <FcEditImage size={30} onClick={() => Router.push(`/p/${book.id}`)} />
-        <FcFullTrash size={30} onClick={() => deleteBook(book.id)} />
+      <div className="book-div">
+        <h2>{book.name}</h2>
+        <small>
+          <i>
+            Written By <b>{authorName}</b>
+          </i>
+        </small>
+      </div>
+      <div className="book-div book-buttons">
+        {book.user_writer?.email === user?.email && (
+          <>
+            <button>
+              Edit{" "}
+              <FcEditImage
+                size={30}
+                onClick={() => Router.push(`/p/${book.id}`)}
+              />
+            </button>
+            <button>
+              Delete
+              <FcFullTrash size={30} onClick={() => deleteBook(book.id)} />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
