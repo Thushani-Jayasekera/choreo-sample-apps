3dfe5a910d7860fdffefe35769f5ebb493c063c9
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2023-09-22 08:43:28 +0530

    Nextauth to previous state

--- web-apps/nextJS/nextjs-new-readinglist-front-end/next.config.js
+++ web-apps/nextJS/nextjs-new-readinglist-front-end/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // output:'standalone',
   publicRuntimeConfig: {
     NEXT_PUBLIC_SERVICE_URL: process.env.NEXT_PUBLIC_SERVICE_URL,
   },
